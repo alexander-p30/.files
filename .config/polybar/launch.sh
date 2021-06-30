012f630dd5db1f4346c76a957eb8c856ec510021
alexander-p30 <alexanderp30e@gmail.com>
2021-06-30 17:57:22 -0300

    launch polybar on all monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,5 +9,8 @@ killall -q polybar
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
 
+DISPLAY2="$(xrandr -q | grep 'HDMI1\|DVI-I-1' | cut -d ' ' -f1)"
+[[ ! -z $DISPLAY2 ]] && MONITOR=$DISPLAY2 polybar mybar &
+
 echo "Bars launched..."
 
