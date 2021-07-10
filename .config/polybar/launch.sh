6063e4b1e317eb76a6a73f5846ad812e434a30e2
alexander-p30 <alexanderp30e@gmail.com>
2021-07-10 12:03:10 -0300

    improve polybar startup with multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,16 +1,6 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
 
-# Launch bar1
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
-
-DISPLAY2="$(xrandr -q | grep 'HDMI1\|DVI-I-1' | cut -d ' ' -f1)"
-[[ ! -z $DISPLAY2 ]] && MONITOR=$DISPLAY2 polybar mybar &
-
-echo "Bars launched..."
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  MONITOR=$m polybar --reload mybar &
+done
 
