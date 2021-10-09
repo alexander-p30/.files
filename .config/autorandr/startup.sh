8fb7c1462e8bb7f5ade65e9ec1e73c51f5926b08
alexander-p30 <alexanderp30e@gmail.com>
2021-10-09 18:57:17 -0300

    improve autorandr startup script

--- .config/autorandr/startup.sh
+++ .config/autorandr/startup.sh
@@ -1,1 +1,8 @@
-autorandr -l $(autorandr --detected)
+DETECTED=$(autorandr --detected)
+CURRENT=$(autorandr --current)
+
+if [[ $DETECTED != $CURRENT ]]; then
+  autorandr -l $(autorandr --detected)
+fi
+
+nitrogen --restore
