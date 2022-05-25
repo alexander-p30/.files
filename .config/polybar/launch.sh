9bd3d18110812a4d8c1699acc288fb9ba3f86c98
alexander-p30 <alexanderp30e@gmail.com>
2022-05-25 19:23:56 -0300

    several (but minor) tweaks

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,6 +1,11 @@
 killall -q polybar
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+MONITOR=$PRIMARY TRAY_POS=right polybar  --reload mybar &
+
+for m in $OTHERS; do
   MONITOR=$m polybar --reload mybar &
 done
 
